6ad07735d4924cb5d93d56d242d9f9f9874e677a
Chen-Yu Pao <windperson@gmail.com>
2020-12-08 19:03:41 +0800

    Add project files.

---
+++ MyCompLib/MyComp.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace MyCompLib
+{
+    public partial class MyComp : ComponentBase
+    {
+        private string imageUrl =
+    "https://dummyimage.com/286x180/8EB1C7/FEFDFF.png&text=Awesome+Product";
+        
+        [Parameter]
+        public string ProductName { get; set; } = "Awesome Product";
+        [Parameter]
+        public string ProductDescription { get; set; } = 
+            "You won't believe how great this product is until you actually use it yourself.";
+        [Parameter]
+        public EventCallback<string> TextChanged { get; set; }
+
+        [Inject]
+        private ExampleJsInterop exampleJsInterop { get; set; }
+
+        private async Task UpdateProductName()
+        {
+            // Call JS interop.
+            var text = await exampleJsInterop.Prompt(ProductName);
+            // Trigger the changed event.
+            await TextChanged.InvokeAsync(text);
+            // Set the property value.
+            ProductName = text;
+        }
+    }
+}
